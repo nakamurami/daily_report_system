d66d9dc16a493bd052c2142874f0b4975fd6bd4c
m.nakamura <na.m.22222285@gmail.com>
2019-05-30 01:33:48 +0900

    Add Report class

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -40,10 +40,11 @@ public class LoginFilter implements Filter {
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String context_path = ((HttpServletRequest)request).getContextPath();
-        String servlet_path = ((HttpServletRequest)request).getContextPath();
+        String servlet_path = ((HttpServletRequest)request).getServletPath();
 
         if(!servlet_path.matches("/css.*")) {
             HttpSession session = ((HttpServletRequest)request).getSession();
+
 
             Employee e = (Employee)session.getAttribute("login_employee");
 
@@ -59,21 +60,17 @@ public class LoginFilter implements Filter {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/");
                     return;
                 }
-
             } else {
 
                 if(e != null) {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/");
                     return;
-
                 }
             }
         }
 
         chain.doFilter(request, response);
     }
-
-
 
     /**
      * @see Filter#init(FilterConfig)
